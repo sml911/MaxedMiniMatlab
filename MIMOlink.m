ae23f3427d64110f1b1349517f591c215285e7aa
Leone Stephen <leone@comm08.ee.cooper.edu>
2016-04-14 15:22:56 -0400

    loop

--- MIMOlink.m
+++ MIMOlink.m
@@ -9,58 +9,66 @@ numTx = 2;
 T = 1e-5;
 dopshift = 0;
 
-bits = randi([0,1],numTx,N);
-msg = zeros(2,size(bits,2)/k);
-for ii = 1:numTx
-    msg(ii,:) = bi2de(reshape(bits(ii,:),k,size(bits,2)/k).','left-msb')';
+numIter = 100;
+berPlain = zeros(1,numIter);
+berPrecode = zeros(1,numIter);
+berZF = zeros(1,numIter);
+berMMSE = zeros(1,numIter);
+
+for jj = 1:numIter
+    bits = randi([0,1],numTx,N);
+    msg = zeros(2,size(bits,2)/k);
+    for ii = 1:numTx
+        msg(ii,:) = bi2de(reshape(bits(ii,:),k,size(bits,2)/k).','left-msb')';
+    end
+    xTilde = qammod(msg,M);
+    xTilde = xTilde./sqrt(var(xTilde(:))); %normalize power
+
+    chan1 = rayleighchan(T,dopshift);
+    chan2 = rayleighchan(T,dopshift);
+    chan3 = rayleighchan(T,dopshift);
+    chan4 = rayleighchan(T,dopshift);
+
+    H = [chan1.PathGains,chan2.PathGains;chan3.PathGains,chan4.PathGains];
+    [U,S,V] = svd(H);
+
+
+    noise = wgn(size(xTilde,1),size(xTilde,2),N0,'complex');
+
+    %PLAIN MIMO
+    yPlain = H*xTilde;
+    yPlainN = yPlain + noise;
+    msgRxPlain = qamdemod(yPlainN,M);
+    [~,berPlain(jj)] = biterr(msg,msgRxPlain);
+
+    %PRECODING
+    xPrecode = V'*xTilde;
+    yPrecode = H*xPrecode;
+    yPrecodeN = yPrecode + noise;
+    scatterplot(yPrecodeN(:));
+    title('Precode - Before');
+    yTilde = U' * yPrecodeN;
+    scatterplot(yTilde(:));
+    title('Precode - After');
+    msgRxPrecode = qamdemod(yTilde,M);
+    [~,berPrecode(jj)] = biterr(msg,msgRxPrecode);
+    %ZEROFORCING
+    W = pinv(H);
+    scatterplot(yPlainN(:));
+    title('ZF - Before');
+    yZFN = W*yPlainN;
+    scatterplot(yZFN(:));
+    title('ZF - After');
+    msgRxZF = qamdemod(yZFN,M);
+    [~,berZF(jj)] = biterr(msg,msgRxZF);
+
+    %MMSE
+    W = inv(H'*H + N0)*H';
+    scatterplot(yPlainN(:));
+    title('MMSE - Before');
+    yMMSEN = W*yPlainN;
+    scatterplot(yMMSEN(:));
+    title('MMSE - After');
+    msgRxMMSE = qamdemod(yMMSEN,M);
+    [~,berMMSE(jj)] = biterr(msg,msgRxMMSE);
 end
-xTilde = qammod(msg,M);
-xTilde = xTilde./sqrt(var(xTilde(:))); %normalize power
-
-chan1 = rayleighchan(T,dopshift);
-chan2 = rayleighchan(T,dopshift);
-chan3 = rayleighchan(T,dopshift);
-chan4 = rayleighchan(T,dopshift);
-
-H = [chan1.PathGains,chan2.PathGains;chan3.PathGains,chan4.PathGains];
-[U,S,V] = svd(H);
-
-
-noise = wgn(size(xTilde,1),size(xTilde,2),N0,'complex');
-
-%PLAIN MIMO
-yPlain = H*xTilde;
-yPlainN = yPlain + noise;
-msgRxPlain = qamdemod(yPlainN,M);
-[~,berPlain] = biterr(msg,msgRxPlain)
-
-%PRECODING
-xPrecode = V'*xTilde;
-yPrecode = H*xPrecode;
-yPrecodeN = yPrecode + noise;
-scatterplot(yPrecodeN(:));
-title('Precode - Before');
-yTilde = U' * yPrecodeN;
-scatterplot(yTilde(:));
-title('Precode - After');
-msgRxPrecode = qamdemod(yTilde,M);
-[~,berPrecode] = biterr(msg,msgRxPrecode)
-%ZEROFORCING
-W = pinv(H);
-scatterplot(yPlainN(:));
-title('ZF - Before');
-yZFN = W*yPlainN;
-scatterplot(yZFN(:));
-title('ZF - After');
-msgRxZF = qamdemod(yZFN,M);
-[~,berZF] = biterr(msg,msgRxZF)
-
-%MMSE
-W = inv(H'*H + N0)*H';
-scatterplot(yPlainN(:));
-title('MMSE - Before');
-yMMSEN = W*yPlainN;
-scatterplot(yMMSEN(:));
-title('MMSE - After');
-msgRxMMSE = qamdemod(yMMSEN,M);
-[~,berMMSE] = biterr(msg,msgRxMMSE)
